5b140bfc18213f3f5b9852d9100b29807149d3f2
niraj003 <104854542+niraj003@users.noreply.github.com>
2023-08-08 16:03:09 +0530

    v1.0t

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const roomHash = location.hash.substring(1);
   
 // TODO: Replace with your own channel ID
 var url =`https://video-calling-smoky.vercel.app/#${roomHash}`;
-document.getElementById("url").innerHTML = ` <span>Url: </span>
-<span id="myInput">${url}</span>`;
+// document.getElementById("url").innerHTML = ` <span>Url: </span>
+// <span id="myInput">${url}</span>`;
 const drone = new ScaleDrone('W7QmZeNjHpNTNFUb');
 
 // whatsapp shear
